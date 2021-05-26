9bf61e0957d1010bc89e6ffb8662b96047fa2660
Anjou Stiles <anjou.stiles@gmail.com>
2021-05-26 16:12:36 -0500

    Enforce black style

--- onthelam/__init__.py
+++ onthelam/__init__.py
@@ -1,59 +1,68 @@
 import ast
 
 OP_MAP = {
-    '+': ast.Add(),
-    '-': ast.Sub(),
-    '*': ast.Mult(),
-    '@': ast.MatMult(),
-    '/': ast.Div(),
-    '//': ast.FloorDiv(),
-    '%': ast.Mod(),
+    "+": ast.Add(),
+    "-": ast.Sub(),
+    "*": ast.Mult(),
+    "@": ast.MatMult(),
+    "/": ast.Div(),
+    "//": ast.FloorDiv(),
+    "%": ast.Mod(),
 }
 
 # Referenced from https://docs.python.org/3/reference/expressions.html#operator-precedence
 OP_PRECEDENCE = {
-    'bool': 1,
-    'or': 2,
-    'and': 3,
-    'not': 4,
-    'in': 5,
-    'is': 5,
-    '<': 5,
-    '<=': 5,
-    '==': 5,
-    '!=': 5,
-    '>=': 5,
-    '>': 5,
-    '|': 6,
-    '^': 7,
-    '&': 8,
-    '<<': 9,
-    '>>': 9,
-    '+': 10,
-    '-': 10,
-    '*': 11,
-    '@': 11,
-    '/': 11,
-    '//': 11,
-    '%': 11,
-    '+x': 12,
-    '-x': 12,
-    '~x': 12,
-    '**': 13,
-    'await': 14,
-    '[]': 15,
-    '.': 15,
-    'x()': 15
+    "bool": 1,
+    "or": 2,
+    "and": 3,
+    "not": 4,
+    "in": 5,
+    "is": 5,
+    "<": 5,
+    "<=": 5,
+    "==": 5,
+    "!=": 5,
+    ">=": 5,
+    ">": 5,
+    "|": 6,
+    "^": 7,
+    "&": 8,
+    "<<": 9,
+    ">>": 9,
+    "+": 10,
+    "-": 10,
+    "*": 11,
+    "@": 11,
+    "/": 11,
+    "//": 11,
+    "%": 11,
+    "+x": 12,
+    "-x": 12,
+    "~x": 12,
+    "**": 13,
+    "await": 14,
+    "[]": 15,
+    ".": 15,
+    "x()": 15,
 }
 
+
 class LambdaBuilder:
-    def __init__(self, body=None, body_str=None, parent=None, closed_values=None, closed_count=0, precedence=100):
+    def __init__(
+        self,
+        body=None,
+        body_str=None,
+        parent=None,
+        closed_values=None,
+        closed_count=0,
+        precedence=100,
+    ):
         if not body:
-            body = ast.Name(id='_', ctx=ast.Load())
-            body_str = '_'
+            body = ast.Name(id="_", ctx=ast.Load())
+            body_str = "_"
         self.__body = body
         if not body_str:
-            raise ValueError('body_str must be present if body is provided')
+            raise ValueError("body_str must be present if body is provided")
         self.__body_str = body_str
         self.__parent = parent
         if not closed_values:
@@ -63,22 +72,26 @@ class LambdaBuilder:
         self.__precedence = precedence
 
     def __repr__(self):
-        return '_ -> ' + self.__body_str
+        return "_ -> " + self.__body_str
 
     def __op(self, op, other):
-        body_str = f'({self.__body_str})' if self.__precedence < OP_PRECEDENCE[op] else self.__body_str
+        body_str = (
+            f"({self.__body_str})"
+            if self.__precedence < OP_PRECEDENCE[op]
+            else self.__body_str
+        )
 
         new = LambdaBuilder(
             ast.BinOp(
                 left=self.__body,
                 op=OP_MAP[op],
                 right=ast.Subscript(
-                    value=ast.Name(id='closure', ctx=ast.Load()),
+                    value=ast.Name(id="closure", ctx=ast.Load()),
                     slice=ast.Constant(value=self.__closure_size),
-                    ctx=ast.Load()
-                )
+                    ctx=ast.Load(),
+                ),
             ),
-            body_str=(f'{body_str} {op} {repr(other)}'),
+            body_str=(f"{body_str} {op} {repr(other)}"),
             parent=self,
             closed_values=[other],
             closed_count=self.__closure_size,
@@ -87,19 +100,23 @@ class LambdaBuilder:
         return new
 
     def __rop(self, op, other):
-        body_str = f'({self.__body_str})' if self.__precedence <= OP_PRECEDENCE[op] else self.__body_str
+        body_str = (
+            f"({self.__body_str})"
+            if self.__precedence <= OP_PRECEDENCE[op]
+            else self.__body_str
+        )
 
         new = LambdaBuilder(
             ast.BinOp(
                 left=ast.Subscript(
-                    value=ast.Name(id='closure', ctx=ast.Load()),
+                    value=ast.Name(id="closure", ctx=ast.Load()),
                     slice=ast.Constant(value=self.__closure_size),
-                    ctx=ast.Load()
+                    ctx=ast.Load(),
                 ),
                 op=OP_MAP[op],
                 right=self.__body,
             ),
-            body_str=(f'{repr(other)} {op} {body_str}'),
+            body_str=(f"{repr(other)} {op} {body_str}"),
             parent=self,
             closed_values=[other],
             closed_count=self.__closure_size,
@@ -108,46 +125,46 @@ class LambdaBuilder:
         return new
 
     def __add__(self, other):
-        return self.__op('+', other)
+        return self.__op("+", other)
 
     def __radd__(self, other):
-        return self.__rop('+', other)
+        return self.__rop("+", other)
 
     def __sub__(self, other):
-        return self.__op('-', other)
+        return self.__op("-", other)
 
     def __rsub__(self, other):
-        return self.__rop('-', other)
+        return self.__rop("-", other)
 
     def __mul__(self, other):
-        return self.__op('*', other)
+        return self.__op("*", other)
 
     def __rmul__(self, other):
-        return self.__rop('*', other)
+        return self.__rop("*", other)
 
     def __truediv__(self, other):
-        return self.__op('/', other)
+        return self.__op("/", other)
 
     def __rtruediv__(self, other):
-        return self.__rop('/', other)
+        return self.__rop("/", other)
 
     def __floordiv__(self, other):
-        return self.__op('//', other)
+        return self.__op("//", other)
 
     def __rfloordiv__(self, other):
-        return self.__rop('//', other)
+        return self.__rop("//", other)
 
     def __mod__(self, other):
-        return self.__op('%', other)
+        return self.__op("%", other)
 
     def __rmod__(self, other):
-        return self.__rop('%', other)
+        return self.__rop("%", other)
 
     def __matmul__(self, other):
-        return self.__op('@', other)
+        return self.__op("@", other)
 
     def __rmatmul__(self, other):
-        return self.__rop('@', other)
+        return self.__rop("@", other)
 
     def __call__(self, arg):
         fn = self.__compile()
@@ -157,7 +174,7 @@ class LambdaBuilder:
         ast_object = ast.Expression(
             ast.Lambda(
                 args=ast.arguments(
-                    posonlyargs=[ast.arg(arg='_')],
+                    posonlyargs=[ast.arg(arg="_")],
                     args=[],
                     kwonlyargs=[],
                     kw_defaults=[],
@@ -167,11 +184,9 @@ class LambdaBuilder:
             )
         )
         code = compile(
-            ast.fix_missing_locations(ast_object),
-            '<LambdaBuilder>',
-            mode='eval'
+            ast.fix_missing_locations(ast_object), "<LambdaBuilder>", mode="eval"
         )
-        return eval(code, {'closure': self.__assemble_closure()})
+        return eval(code, {"closure": self.__assemble_closure()})
 
     def __assemble_closure(self):
         node = self
@@ -181,8 +196,10 @@ class LambdaBuilder:
             closure[:0] = node.__closure
         if len(closure) != self.__closure_size:
             raise RuntimeError(
-                f'Lambda closure corrupt. Expected size {self.__closure_size}'
-                f' but found {len(closure)} stored values.', closure)
+                f"Lambda closure corrupt. Expected size {self.__closure_size}"
+                f" but found {len(closure)} stored values.",
+                closure,
+            )
         return closure
 
 
--- tests/test_lambdabuilder.py
+++ tests/test_lambdabuilder.py
@@ -8,7 +8,7 @@ from onthelam import LambdaBuilder, _
 
 def test_simple_identity():
     fn = _
-    assert repr(fn) == '_ -> _'
+    assert repr(fn) == "_ -> _"
     assert fn(5) == 5
 
 
@@ -16,33 +16,33 @@ OBJ = MagicMock()
 
 
 @pytest.mark.parametrize(
-    ('expr', 'body_str', 'arg', 'result'),
+    ("expr", "body_str", "arg", "result"),
     [
-        (lambda: _ + 5, '_ + 5', 2, 2 + 5),
-        (lambda: 5 + _, '5 + _', 2, 5 + 2),
-        (lambda: _ - 5, '_ - 5', 2, 2 - 5),
-        (lambda: 5 - _, '5 - _', 2, 5 - 2),
-        (lambda: _ * 2, '_ * 2', 3, 3 * 2),
-        (lambda: 2 * _, '2 * _', 3, 2 * 3),
-        (lambda: _ / 2, '_ / 2', 5, 5 / 2),
-        (lambda: 2 / _, '2 / _', 5, 2 / 5),
-        (lambda: _ // 2, '_ // 2', 5, 5 // 2),
-        (lambda: 2 // _, '2 // _', 5, 2 // 5),
-        (lambda: _ % 2, '_ % 2', 5, 5 % 2),
-        (lambda: 2 % _, '2 % _', 5, 2 % 5),
-        (lambda: _ @ 2, '_ @ 2', OBJ, OBJ @ 2),
-        (lambda: 2 @ _, '2 @ _', OBJ, 2 @ OBJ),
+        (lambda: _ + 5, "_ + 5", 2, 2 + 5),
+        (lambda: 5 + _, "5 + _", 2, 5 + 2),
+        (lambda: _ - 5, "_ - 5", 2, 2 - 5),
+        (lambda: 5 - _, "5 - _", 2, 5 - 2),
+        (lambda: _ * 2, "_ * 2", 3, 3 * 2),
+        (lambda: 2 * _, "2 * _", 3, 2 * 3),
+        (lambda: _ / 2, "_ / 2", 5, 5 / 2),
+        (lambda: 2 / _, "2 / _", 5, 2 / 5),
+        (lambda: _ // 2, "_ // 2", 5, 5 // 2),
+        (lambda: 2 // _, "2 // _", 5, 2 // 5),
+        (lambda: _ % 2, "_ % 2", 5, 5 % 2),
+        (lambda: 2 % _, "2 % _", 5, 2 % 5),
+        (lambda: _ @ 2, "_ @ 2", OBJ, OBJ @ 2),
+        (lambda: 2 @ _, "2 @ _", OBJ, 2 @ OBJ),
     ],
 )
 def test_basic_ops(expr, body_str, arg, result):
     fn = expr()
-    assert repr(fn) == f'_ -> {body_str}'
+    assert repr(fn) == f"_ -> {body_str}"
     assert fn(arg) == result
 
 
 def test_missing_body_str():
     with pytest.raises(ValueError):
-        fn = LambdaBuilder(body=ast.parse('_', mode='eval'))
+        fn = LambdaBuilder(body=ast.parse("_", mode="eval"))
 
 
 def test_corrupt_closure():
@@ -52,30 +52,30 @@ def test_corrupt_closure():
 
 
 def test_add_multiple():
-    fn = _ + ', hello' + ' world'
+    fn = _ + ", hello" + " world"
     assert repr(fn) == "_ -> _ + ', hello' + ' world'"
-    assert fn('hi') == 'hi, hello world'
+    assert fn("hi") == "hi, hello world"
 
 
 def test_add_both():
     fn = 1 + _ + 4
-    assert repr(fn) == '_ -> 1 + _ + 4'
+    assert repr(fn) == "_ -> 1 + _ + 4"
     assert fn(2) == 7
 
 
 def test_arithmetic_precedence_no_parens():
     fn = 1 + _ * 2
-    assert repr(fn) == '_ -> 1 + _ * 2'
+    assert repr(fn) == "_ -> 1 + _ * 2"
     assert fn(3) == 7
 
 
 def test_arithmetic_precedence_parens():
     fn = 1 + (_ + 2)
-    assert repr(fn) == '_ -> 1 + (_ + 2)'
+    assert repr(fn) == "_ -> 1 + (_ + 2)"
     assert fn(3) == 7
 
 
 def test_arithmetic_precedence_parens():
     fn = (_ + 1) * 2
-    assert repr(fn) == '_ -> (_ + 1) * 2'
+    assert repr(fn) == "_ -> (_ + 1) * 2"
     assert fn(3) == 8
